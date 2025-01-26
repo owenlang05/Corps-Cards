d81605fe170cb7c9406311b1a9020ec79264a65b
Owen Lang <owen.lang146@gmail.com>
2025-01-26 15:55:25 -0500

    fixed flashcards

--- client/src/components/FlashcardList.jsx
+++ client/src/components/FlashcardList.jsx
@@ -4,9 +4,11 @@ import Flashcard from './Flashcard'
 export default function FlashcardList({flashcards}) {
     const [activeIndex, setActiveIndex] = useState(0)
     const [flashcard, setFlashcard] = useState(flashcards[0])
+    var index = 0
     const onChangeIndex  = (value) => {
-        setActiveIndex(Math.max(0,Math.min(activeIndex+value, flashcards.length-1)))
-        setFlashcard(flashcards[activeIndex])
+        index = Math.max(0,Math.min(activeIndex+value, flashcards.length-1))
+        setActiveIndex(index)
+        setFlashcard(flashcards[index])
     }
     return (
         <>
